cc7486f99c4cd908c48c43b65c81d3cdbe39b089
Mike Foster <i.am.mike.foster@gmail.com>
2024-05-20 20:11:11 +0000

    RED: test_add

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -2,3 +2,23 @@ from assets.python.pypkg import testlib
 
 def test_imported():
     assert 'add' in dir(testlib)
+
+add_pyi = '''
+def add(left, right):
+    """
+    Adds two numbers together
+
+    Has a multi-line docstring
+    """
+'''
+
+def test_add():
+    add_pyi = '''
+def add(left, right):
+    """
+    Adds two numbers together
+
+    Has a multi-line docstring
+    """
+'''
+    assert generate(testlib, "add") == add_pyi
