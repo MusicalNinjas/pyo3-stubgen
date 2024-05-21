cc7359772021612e43aa4257a31de3f23a17311d
Mike Foster <i.am.mike.foster@gmail.com>
2024-05-21 18:57:09 +0000

    typos

--- pyo3_stubgen/__init__.py
+++ pyo3_stubgen/__init__.py
@@ -5,7 +5,7 @@ It is designed to work for extension modules created in rust with pyo3 but shoul
 modules which include a `__text_signature__` and optionally a `__doc__` attribute for functions.
 
 The package containing the module must be installed in the current virtual environment, but does not need to be
-imported before runnin `pyo3_stubgen`.
+imported before running `pyo3_stubgen`.
 
 Despite flake8's recommendation to the contrary, `pyo3_stubgen` adds docstrings to the `.pyi` files so that IDEs can
 provide them in hover pop-ups.
