a3f43261abccbb9e9c3eba2d09e94009bd29ca71
Mike Foster <i.am.mike.foster@gmail.com>
2024-05-21 05:58:47 +0000

    move pyi out of pypkg

--- tests/assets/expected.pyi
+++ tests/assets/expected.pyi
@@ -1,19 +1,13 @@
 # ruff: noqa
-def add(left, right):
+def multiline(left, right):
     """
     Adds two numbers together
 
     Has a multi-line docstring
     """
 
-def double(num):
+def minimal(num):
     """Has a one line docstring and one argument"""
-
-def neither():
-    ...
 
 def no_docstring(num):
     ...
-
-def no_signature():
-    """A docstring but no signature"""
